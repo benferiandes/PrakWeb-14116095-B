706e8eef49cae1dd7714ae5d4e36f7b002d58598
benferiandes <benferiandes@gmail.com>
2020-11-14 23:32:04 +0700

    Modul 4
    PHP

---
+++ Pertemuan2/PHP/tugas2.php
@@ -0,0 +1,8 @@
+<?php
+$nama = array("Lanirne", "Aduh", "Qifuat", "Toda", "Anevi", "Samid", "Kifuat");
+sort($nama);
+foreach ($nama as $n) {
+	echo "$n ";
+	echo '<br>';
+}
+?>
---
+++ Pertemuan2/PHP/tugas3.php
@@ -0,0 +1,9 @@
+<?php 
+echo "Berikut adalah Bilangan Prima dari 1-50 : <br>";
+for ($i = 1; $i <= 50; $i++){
+	if ($i%2 ==1) {
+	echo "$i ";
+	echo '<br>';
+	}
+}
+?>
